3fa255b14b5cc383a7964c5c93b0e6e25c883b0f
Luís da Silveira Carneiro Júnior <lscj.ldc@gmail.com>
2025-04-26 12:31:03 -0300

    Aula 1, 2 e 3

--- aula-um.php
+++ aula-um.php
@@ -1,3 +1,17 @@
 <?php
+    # Minhas variáveis
+    $produto = "Carro";
+    $valor = 'Trinta mil reais <br>';
+    $bolean = true;
+    # Minhas chamadas para o usuários
+    # Primeira aula
     echo "Olá mundo";
+    echo "<br> Seja Bem Vindo ao Curso de PHP Aula 1";
+    # Segunda aula, aprendendo variáveis
+    echo "<br> Seja Bem Vindo ao Curso de PHP Aula 2 <br>";
+    echo "O valor do $produto é $valor";
+    echo "<br> $bolean <br>";
+    # Terceira aula, aprendendo constant
+    define("MENSAGEM", "Seja Bem Vindo ao Curso de PHP Aula 3 <br>");
+    echo MENSAGEM;
     ?>
